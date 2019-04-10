09bfca521bea935dc63e90aa2ea754a1ddc192e3
tranina-olesya <tranina-olesya@users.noreply.github.com>
2019-04-10 09:11:46 +0300

    Kiwi response convert (round trips added)

--- app/src/main/java/ru/vsu/aviatickets/ticketssearch/providers/KiwiProviderAPI.java
+++ app/src/main/java/ru/vsu/aviatickets/ticketssearch/providers/KiwiProviderAPI.java
@@ -20,6 +20,8 @@ import ru.vsu.aviatickets.ticketssearch.models.Trip;
 import ru.vsu.aviatickets.ticketssearch.models.kiwi.Datum;
 import ru.vsu.aviatickets.ticketssearch.models.kiwi.KiwiResponse;
 import ru.vsu.aviatickets.ticketssearch.models.kiwi.Route;
+import ru.vsu.aviatickets.ticketssearch.models.kiwi.SearchParams;
+import ru.vsu.aviatickets.ticketssearch.models.kiwi.Seats;
 
 public class KiwiProviderAPI extends ProviderAPI<KiwiAPI> {
     public KiwiProviderAPI() {
@@ -28,16 +30,19 @@ public class KiwiProviderAPI extends ProviderAPI<KiwiAPI> {
 
     @Override
     public void getTickets(TicketsCallback callback) {
-        getTicketsApi().getTickets("VOZ", "MOW", "18/04/2019", "18/04/2019", "20/04/2019", "20/04/2019","oneway").enqueue(new Callback<KiwiResponse>() {
+        getTicketsApi().getTickets("VOZ", "MOW", "18/04/2019", "18/04/2019", "20/04/2019", "20/04/2019", "round").enqueue(new Callback<KiwiResponse>() {
             @Override
             public void onResponse(Call<KiwiResponse> call, Response<KiwiResponse> response) {
-                if (response.body() != null)
-                    convertResponseToTrip(response.body());
+                if (response.body() != null) {
+                    callback.onGet(convertResponseToTrip(response.body()));
+                } else {
+                    callback.onFail();
+                }
             }
 
             @Override
             public void onFailure(Call<KiwiResponse> call, Throwable t) {
-
+                callback.onFail();
             }
         });
     }
@@ -52,9 +57,9 @@ public class KiwiProviderAPI extends ProviderAPI<KiwiAPI> {
         return retrofit.create(KiwiAPI.class);
     }
 
-    private List<Trip> convertResponseToTrip(KiwiResponse response){
+    private List<Trip> convertResponseToTrip(KiwiResponse response) {
         List<Trip> trips = new ArrayList<>();
-        for (Datum data: response.getData()){
+        for (Datum data : response.getData()) {
             Trip trip = new Trip();
 
             List<PriceLink> priceLinks = new ArrayList<>();
@@ -63,39 +68,54 @@ public class KiwiProviderAPI extends ProviderAPI<KiwiAPI> {
             PriceLink priceLink = new PriceLink(agents, data.getPrice(), data.getDeepLink());
             priceLinks.add(priceLink);
             trip.setPriceLinks(priceLinks);
+            trip.setOutbound(formFlight(response.getSearchParams(), data, 0));
+            trip.setInbound(formFlight(response.getSearchParams(), data, 1));
 
-            Flight outbound = new Flight();
-            outbound.setAdultsCount(response.getSearchParams().getSeats().getAdults());
-            outbound.setChildrenCount(response.getSearchParams().getSeats().getChildren());
-            outbound.setInfantsCount(response.getSearchParams().getSeats().getInfants());
-            outbound.setDuration(data.getDuration().getDeparture());
-            outbound.setOutboundDate(new Date(data.getATime()*1000L));
-            outbound.setInboundDate(new Date(data.getDTime()*1000L));
-            outbound.setOrigin(new Place(data.getFlyFrom(), null, data.getCityFrom()));
-            outbound.setDestination(new Place(data.getFlyTo(), null, data.getCityTo()));
-            outbound.setFlightParts(formTicketsList(data, 0));
-
+            trips.add(trip);
         }
         return trips;
     }
 
-    private List<Route> getRoutesByDirection(List<Route> routes, int direction){
-        return routes.stream().filter(r->r.getReturn()==direction).collect(Collectors.toList());
+    private List<Route> getRoutesByDirection(List<Route> routes, int direction) {
+        return routes.stream().filter(r -> r.getReturn() == direction).collect(Collectors.toList());
     }
 
-    private List<Ticket> formTicketsList(Datum data, int direction){
+    private List<Ticket> formTicketsList(SearchParams searchParams, Datum data, int direction) {
         List<Ticket> tickets = new ArrayList<>();
-        for (Route route: getRoutesByDirection(data.getRoute(), direction)){
+        for (Route route : getRoutesByDirection(data.getRoute(), direction)) {
             Ticket ticket = new Ticket();
-            Carrier carrier = new Carrier(route.getOperatingCarrier(), route.getAirline(), null);
-            ticket.setCarrier(carrier);
-            ticket.setOrigin(new Place(route.getFlyFrom(), null, route.getCityFrom()));
-            ticket.setDestination(new Place(route.getFlyTo(), null, route.getCityTo()));
-            ticket.setOutboundDate(new Date(route.getDTime() *1000L));
-            ticket.setInboundDate(new Date(route.getATime() *1000L));
-            ticket.setDuration((int)((route.getDTime() - route.getATime())/(60000L)));
+            ticket.setCarrier(new Carrier(route.getOperatingCarrier(), route.getAirline(), null));
+            ticket.setOrigin(new Place(route.getFlyFrom(), searchParams.getFlyFromType(), route.getCityFrom()));
+            ticket.setDestination(new Place(route.getFlyTo(), searchParams.getToType(), route.getCityTo()));
+            ticket.setOutboundDate(new Date(route.getDTime() * 1000L));
+            ticket.setInboundDate(new Date(route.getATime() * 1000L));
+            ticket.setDuration((int) ((route.getDTime() - route.getATime()) / (60000L)));
             tickets.add(ticket);
         }
         return tickets;
+    }
+
+    private Flight formFlight(SearchParams searchParams, Datum data, int direction) {
+        Flight flight = new Flight();
+        flight.setAdultsCount(searchParams.getSeats().getAdults());
+        flight.setChildrenCount(searchParams.getSeats().getChildren());
+        flight.setInfantsCount(searchParams.getSeats().getInfants());
+
+        if (direction == 0)
+            flight.setDuration(data.getDuration().getDeparture());
+        else
+            flight.setDuration(data.getDuration().getReturn());
+        List<Ticket> flightParts = formTicketsList(searchParams, data, direction);
+        if (flightParts.isEmpty())
+            return null;
+
+        flight.setFlightParts(flightParts);
+        Ticket start = flightParts.get(0);
+        Ticket end = flightParts.get(flightParts.size() - 1);
+        flight.setOutboundDate(start.getOutboundDate());
+        flight.setInboundDate(end.getInboundDate());
+        flight.setOrigin(start.getOrigin());
+        flight.setDestination(end.getDestination());
+        return flight;
     }
 }
