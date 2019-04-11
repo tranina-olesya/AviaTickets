f7bb82034750084950e99105c38427264845b9ce
Nastya Kopylova <nk9515535184@gmail.com>
2019-04-11 12:54:20 +0300

    work with database

--- app/src/main/java/ru/vsu/aviatickets/App.java
+++ app/src/main/java/ru/vsu/aviatickets/App.java
@@ -1,16 +1,27 @@
 package ru.vsu.aviatickets;
 
 import android.app.Application;
+import android.arch.persistence.room.Room;
 
-import ru.vsu.aviatickets.ticketssearch.providers.SkyScannerProviderAPI;
+import ru.vsu.aviatickets.bookmarks.logic.AviaTicketsDatabase;
 
 public class App extends Application {
+    public static App instance;
+    private AviaTicketsDatabase database;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
+        database = Room.databaseBuilder(this,AviaTicketsDatabase.class,"database").build();
+    }
 
-        SkyScannerProviderAPI skyScannerProviderAPI = new SkyScannerProviderAPI();
-        skyScannerProviderAPI.getTickets();
+    public static App getInstance() {
+        return instance;
+    }
+
+    public AviaTicketsDatabase getDatabase(){
+        return database;
     }
 
 }
