3a4270f2f072a36288ec9363df240c6ee6679a0c
tranina-olesya <tranina-olesya@users.noreply.github.com>
2019-04-20 00:02:48 +0300

    Opening previous activity instead of creating a new one

--- app/src/main/java/ru/vsu/aviatickets/ui/ticketresults/TripActivity.java
+++ app/src/main/java/ru/vsu/aviatickets/ui/ticketresults/TripActivity.java
@@ -50,6 +50,18 @@ public class TripActivity extends AppCompatActivity implements TripContractView 
     };
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
