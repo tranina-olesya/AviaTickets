e3f4ab5d26ed58b35d957f5fe0d586a72aa3c3bb
tranina-olesya <tranina-olesya@users.noreply.github.com>
2019-05-03 15:06:25 +0300

    Sort Trips by Transfers Count

--- app/src/main/java/ru/vsu/aviatickets/ticketssearch/sort/SortTrips.java
+++ app/src/main/java/ru/vsu/aviatickets/ticketssearch/sort/SortTrips.java
@@ -1,12 +1,10 @@
 package ru.vsu.aviatickets.ticketssearch.sort;
 
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import ru.vsu.aviatickets.ticketssearch.models.Flight;
-import ru.vsu.aviatickets.ticketssearch.models.Ticket;
 import ru.vsu.aviatickets.ticketssearch.models.Trip;
 
 public class SortTrips {
@@ -24,6 +22,12 @@ public class SortTrips {
                 break;
             case MAX_TIME:
                 sortByMaxTime(trips);
+                break;
+            case MIN_TRANSFERS:
+                sortByMinTransfers(trips);
+                break;
+            case MAX_TRANSFERS:
+                sortByMaxTransfers(trips);
                 break;
         }
     }
@@ -64,8 +68,27 @@ public class SortTrips {
         });
     }
 
+    private static void sortByMinTransfers(List<Trip> trips) {
+        Collections.sort(trips, new Comparator<Trip>() {
+            @Override
+            public int compare(Trip trip1, Trip trip2) {
+                return countTransfers(trip1) - countTransfers(trip2);
+            }
+        });
+    }
+
+    private static void sortByMaxTransfers(List<Trip> trips) {
+        Collections.sort(trips, new Comparator<Trip>() {
+            @Override
+            public int compare(Trip trip1, Trip trip2) {
+                return countTransfers(trip2) - countTransfers(trip1);
+            }
+        });
+    }
+
     private static int sumTime(Trip trip) {
         int time = 0;
+
         Flight outbound = trip.getOutbound();
         if (outbound != null)
             time += outbound.getDuration();
@@ -74,5 +97,18 @@ public class SortTrips {
             time += inbound.getDuration();
 
         return time;
+    }
+
+    private static int countTransfers(Trip trip) {
+        int transfersCount = 0;
+
+        Flight outbound = trip.getOutbound();
+        if (outbound != null)
+            transfersCount += outbound.getFlightParts().size();
+        Flight inbound = trip.getInbound();
+        if (inbound != null)
+            transfersCount += inbound.getFlightParts().size();
+
+        return transfersCount;
     }
 }
